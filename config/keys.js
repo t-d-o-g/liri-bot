1f570b529eca70448c3e9a1d71fb955ce3978a89
Travis Collins <trc229@gmail.com>
2018-07-24 09:28:25 -0400

    Setup basic project structure

---
+++ config/keys.js
@@ -0,0 +1,20 @@
+exports.twitter = {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+exports.omdb = {
+    id: process.env.OMDB_ID,
+    key: process.env.OMDB_KEY 
+};
+
+exports.google = {
+    key: process.env.GOOGLE_KEY
+};
